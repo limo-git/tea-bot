196dc8a285825a2b4b967c80b43e2174a168aeb8
limo-git <Dimpleyadav32@gmail.com>
2026-01-31 15:05:50 +0530

    convert tailwind config to js

--- tealdr/tailwind.config.js
+++ tealdr/tailwind.config.js
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -15,4 +14,3 @@ const config: Config = {
   },
   plugins: [],
 };
-export default config;
